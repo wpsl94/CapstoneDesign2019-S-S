46afe27cb4e5dbe248f54ba355c8f1916465163f
Leeheejin <leinaere@naver.com>
2019-03-19 16:58:58 +0900

    Create basic controller files
    Create basic controller files.

---
+++ backend/src/main/java/com/capstonedesign/backend/controller/CommentController.java
@@ -0,0 +1,18 @@
+package com.capstonedesign.backend.controller;
+
+import com.capstonedesign.backend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class CommentController {
+
+    @Autowired
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+}
---
+++ backend/src/main/java/com/capstonedesign/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.capstonedesign.backend.controller;
+
+import com.capstonedesign.backend.domain.Account;
+import com.capstonedesign.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/register")
+    public void register(@RequestBody Account account) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        account.setPermission("viewer");
+        userService.createNewAccount(account);
+    }
+}
