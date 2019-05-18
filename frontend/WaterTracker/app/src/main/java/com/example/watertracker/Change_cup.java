cecc6b2f5812c80679781784c32e6c45055e6a51
Kim Hee Ju <wpsl94@naver.com>
2019-05-18 23:56:06 +0900

    LinearLayout으로 변경

--- frontend/WaterTracker/app/src/main/java/com/example/watertracker/Change_cup.java
+++ frontend/WaterTracker/app/src/main/java/com/example/watertracker/Change_cup.java
@@ -2,12 +2,33 @@ package com.example.watertracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Change_cup extends AppCompatActivity {
-
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_cup);
+
+        list = (ListView)findViewById(R.id.list_cup);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("cup1");
+        data.add("cup2");
+        data.add("cup3");
+        data.add("cup4");
+        data.add("cup5");
+        adapter.notifyDataSetChanged();
+
     }
 }
+
--- frontend/WaterTracker/app/src/main/java/com/example/watertracker/Set_Push.java
+++ frontend/WaterTracker/app/src/main/java/com/example/watertracker/Set_Push.java
@@ -1,13 +1,34 @@
 package com.example.watertracker;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class Set_Push extends AppCompatActivity {
-
+    private TimePicker mTimePicker;
+    private Calendar mCalendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set__push);
+
+/*
+        mTimePicker = (TimePicker) findViewById(R.id.timePick);
+
+        mCalendar = Calendar.getInstance();
+        int hour, min;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            hour = mTimePicker.getHour();
+            min = mTimePicker.getMinute();
+        }
+        else {
+            hour = mTimePicker.getCurrentHour();
+            min = mTimePicker.getCurrentMinute();
+        }
+*/
     }
+
 }
